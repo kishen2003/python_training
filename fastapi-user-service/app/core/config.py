4b5614c30dbbb04b2613c5aa5e33051588443c0d
kishen2003 <kishenkichenacoumar@gmail.com>
2026-01-16 08:32:36 +0530

    Established Database connection.

---
+++ fastapi-user-service/app/core/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Application
+    APP_NAME: str
+    ENV: str = "development"
+
+    # Database
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:"
+            f"{self.DB_PASSWORD}@{self.DB_HOST}:"
+            f"{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
---
+++ fastapi-user-service/app/db/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+)
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,  # turn True only for debugging SQL
+)
+
+AsyncSessionLocal = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
