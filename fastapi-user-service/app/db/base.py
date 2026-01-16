565112389b3c1e756acdda1606ada4f1830dca94
kishen2003 <kishenkichenacoumar@gmail.com>
2026-01-16 09:07:31 +0530

    Create User Table

--- fastapi-user-service/app/db/base.py
+++ fastapi-user-service/app/db/base.py
@@ -2,3 +2,6 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+# Import all models here so SQLAlchemy can detect them
+from app.models.user import User  # noqa
---
+++ fastapi-user-service/app/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
