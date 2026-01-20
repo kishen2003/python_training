6bc3311f9841f4914c901c9cba88710d1e08cadf
kishen2003 <kishenkichenacoumar@gmail.com>
2026-01-20 13:40:46 +0530

    Refactor multithreading and sequential into separate functions

--- concurrent-api-fetcher/multithreading.py
+++ concurrent-api-fetcher/multithreading.py
@@ -14,38 +14,51 @@ def call_api(url: str, headers: dict = None, timeout_value: int = 10):
     end_time = time.time()
     print(f"{url} responded with status code: {response.status_code} and took {end_time-start_time} seconds\n")
 
-apis = [
-    "https://httpbin.org/delay/2",
-    "https://httpbin.org/delay/3",
-    "https://jsonplaceholder.typicode.com/posts/1",
-    "https://jsonplaceholder.typicode.com/users/1",
-]
 
-print("\n\nMultithreading\n\n")
+def multithreading(apis):
+    print("\n\nMultithreading\n\n")
 
-threads = []
+    threads = []
 
-start_time = time.time()
+    start_time = time.time()
 
-for api in apis:
-    thread = threading.Thread(target=call_api, args=(api,))
-    threads.append(thread)
-    thread.start()
+    for api in apis:
+        thread = threading.Thread(target=call_api, args=(api,))
+        threads.append(thread)
+        thread.start()
 
-for thread in threads:
-    thread.join()
+    for thread in threads:
+        thread.join()
 
-end_time = time.time()
+    end_time = time.time()
 
-print(f"Total Execution Time using Threading: {end_time-start_time} seconds\n\n")
+    print(f"Total Execution Time using Threading: {end_time-start_time} seconds\n\n")
 
-print("\n\nSequential\n\n")
 
-start_time = time.time()
+def sequential(apis):
+    print("\n\nSequential\n\n")
 
-for api in apis:
-    call_api(api)
+    start_time = time.time()
 
-end_time = time.time()
+    for api in apis:
+        call_api(api)
 
-print(f"Total Execution Time using Sequential: {end_time-start_time} seconds\n\n")
+    end_time = time.time()
+
+    print(f"Total Execution Time using Sequential: {end_time-start_time} seconds\n\n")
+
+
+
+def main():
+    apis = [
+        "https://httpbin.org/delay/2",
+        "https://httpbin.org/delay/3",
+        "https://jsonplaceholder.typicode.com/posts/1",
+        "https://jsonplaceholder.typicode.com/users/1",
+    ]
+    multithreading(apis)
+    sequential(apis)
+
+
+if __name__ == "__main__":
+    main()
